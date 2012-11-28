cb3d5e26ad7aaf868d03052e2fd37b001800127b
damiencorpataux <d@mien.ch>
2012-11-28 16:50:02 +0100

    404 question

--- xfm/iaExtRestController.php
+++ xfm/iaExtRestController.php
@@ -163,6 +163,7 @@ class iaExtRestController extends xWebController {
             $this->params
         )->get();
         // Determines wheter to return a 404 status
+        // FIXME: Why not: inhibit the 404 only when 'xquery'?
         $pk = xModel::load($this->model)->primary();
         $fields = array_keys(xModel::load($this->model)->mapping);
         $by_id = in_array($pk, array_keys($this->params)) && !array_intersect($this->params, $fields);
